867271126a0293c5b8b3491db710e01949aa8a47
vedantjk <vedantkodagi2804@gmail.com>
2023-05-22 12:01:56 +0530

    Made precedences data driven

--- mc/CodeAnalysis/Parser.cs
+++ mc/CodeAnalysis/Parser.cs
@@ -61,42 +61,37 @@ namespace mc.CodeAnalysis
             return new SyntaxTree(_diagnostics,expression,endOfFileToken);
         }
         
-        private ExpressionSyntax ParseExpression()
-        {
-               return ParseTerm();
-        }
-
-        private ExpressionSyntax ParseTerm()
-        {
-            var left = ParseFactor();
-
-            while (Current.Kind == SyntaxKind.PlusToken ||
-                  Current.Kind == SyntaxKind.MinusToken)
-            {
-                var operatorToken = NextToken();
-                var right = ParseFactor();
-                left = new BinaryExpressionSyntax(left, operatorToken, right);
-            }
-            return left;
-        }
-
-        private ExpressionSyntax ParseFactor()
+        private ExpressionSyntax ParseExpression(int parentPrecedence = 0)
         {
             var left = ParsePrimaryExpression();
-
-            while (
-                  Current.Kind == SyntaxKind.IntoToken ||
-                  Current.Kind == SyntaxKind.DivideToken)
+            while(true)
             {
+                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                if(precedence == 0 || precedence <= parentPrecedence)
+                    break;
                 var operatorToken = NextToken();
-                var right = ParsePrimaryExpression();
-                left = new BinaryExpressionSyntax(left, operatorToken, right);
+                var right = ParseExpression(precedence);
+                left = new BinaryExpressionSyntax(left,operatorToken,right);
             }
             return left;
         }
 
+        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
+        {
+            switch (kind)
+            {
+                case SyntaxKind.IntoToken:
+                case SyntaxKind.DivideToken:
+                    return 2;
+                case SyntaxKind.PlusToken:
+                case SyntaxKind.MinusToken:
+                    return 1;
+                default:
+                    return 0;
 
-        private ExpressionSyntax ParsePrimaryExpression()
+            }
+        }
+       private ExpressionSyntax ParsePrimaryExpression()
         {
             if(Current.Kind == SyntaxKind.OpenParenToken)
             {
